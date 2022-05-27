d59fdbeb23eee5758624ccb454b113e610ea025e
kazuki egashira <kzk.08.12.co.jp@gmail.com>
2022-05-27 08:20:06 +0000

    add: feed forward

---
+++ layers/feed_forward.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FFN(nn.Module):
+    def __init__(self, d_model: int, d_ff: int) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, d_ff)
+        self.linear2 = nn.Linear(d_ff, d_model)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        out = self.linear1(x)
+        out = self.linear2(x)
+        return out
